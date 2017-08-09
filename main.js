017f4d1a66b9dcf03831f57d9af8313024a6739a
Angela Vogler <angelavogler4@gmail.com>
2017-08-09 15:23:48 -0400

    added search function in JS to return search results and display 50 tracks

--- main.js
+++ main.js
@@ -7,3 +7,40 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+//define global variables and assign DOM elements
+let searchBtn = document.querySelector("#search-btn");
+let userInput = document.querySelector("#search-bar");
+let player = document.querySelector('.player');
+
+searchBtn.addEventListener("click", function () {
+  fetch (`https://itunes.apple.com/search?term=${userInput.value}`)
+    .then(convertFromJson)
+    .then(showArtistDeets);
+});
+
+function convertFromJson(artist) {
+  return artist.json();
+
+}
+
+function showArtistDeets(artist) {
+  console.log(artist);
+  let results = document.querySelector('.results');
+  let artistDisplay = '';
+
+  for (let i = 0; i < artist.results.length; i++) {
+
+    let artistResults = artist.results[i];
+
+    let artistCode = `
+      <div class="track-info">
+        <img src="${artistResults.artworkUrl100}">
+        <h4>${artistResults.trackName}</h4>
+        <h3>${artistResults.artistName}</h3>
+      </div>
+    `;
+    artistDisplay += artistCode;
+  }
+  results.innerHTML += artistDisplay;
+}
